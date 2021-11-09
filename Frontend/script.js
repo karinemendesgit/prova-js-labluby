3eb237897a382c87b57870825e496e579d82a4c8
Karine Mendes <karinemendessantos@hotmail.com>
2021-11-08 21:01:17 -0300

    Feat: make new updates to the app

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,41 +1,52 @@
-(function($) {
-    'use strict';
+(function(window, document) {
+  'use strict';
 
-    function app() {
-        return {
-          init: function() {
-
-        },
-
-    getBet: function() {
-        var ajax = new XMLHttpRequest();
-        ajax.open('GET', 'http://localhost:3000/');
-        ajax.send();
-        ajax.onreadystatechange = function() {
-          if (ajax.readyState === 4) {
-            var data = JSON.parse(ajax.responseText);            
-          }
-        };
+  function app() {
+    return {
+      init: function() {
+        this.selectBet()
       },
 
-      postBet: function() {
-        var ajax = new XMLHttpRequest();        
-        ajax.open('POST', 'http://localhost:3000/');
-        ajax.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-        ajax.send(`image=${car.image}&brandModel=${car.brandModel}&year=${car.year}&plate=${car.plate}&color=${car.color}`);
-        ajax.onreadystatechange = function() {
-          if (ajax.readyState === 4) {
-            console.log('Carro cadastrado!', ajax.responseText);
-          }
+      selectBet: function() {
+        var ajax = new XMLHttpRequest();
+        ajax.open('GET', './games.json');
+        ajax.send();
+        ajax.addEventListener('readystatechange', this.getBetRule)
+      },
+
+      getBetRule: function() {
+        if(this.status === 200 && this.readyState === 4) {
+          let data = JSON.parse(this.responseText);
+          app.firstActionsState(data.types);
         }
       },
 
-      deleteBet: function (item) {
-        var ajax = new XMLHttpRequest();
-        ajax.open('DELETE', 'http://localhost:3000/');
-        ajax.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-        ajax.send({id});
-        window.location.reload()
+      firstActionsState: function (dataGame) {
+        const selectBetType = document.querySelector('.bet-types');
+        const btnClear = document.querySelector('[data-js="clear-game"]');
+        const btnRandom = document.querySelector('[data-js="random-game"]');
+        const btnAdd = document.querySelector('[data-js="add-game"]');
+        this.cartInitialLayout();
+        app.selectBet(dataGame, selectBetType)
+        btnClear.addEventListener('click', this.clearBet);
+        btnRandom.addEventListener('click', this.randomGame);
+        btnAdd.addEventListener('click', this.setNumbersInCart);
+      },
+
+      cartInitialLayout: function() {
+        let $itemsCart = document.querySelector('[data-js="game-list"]');
+        let 
+      },
+
+      selectBet: function name(dataGame, selectBetType) {
+        dataGame.map(function (item, index) {
+          let selectNumbers = [];
+          let $button
+        })
+      },      
+
+      clearBet: function () {
+
       },
 
       isReady: function() {
@@ -46,4 +57,4 @@
 
 app().init();
 
-})(window.DOM);
+})(window, document);
